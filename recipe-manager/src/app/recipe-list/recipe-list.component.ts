3cac61642afb79d39d5c2d42f589b2fd8144e7c7
RalucaSpt <spatarularisaraluca@gmail.com>
2025-03-12 16:44:15 +0200

    update recipe list component with Angular Material for improved layout and styling

--- recipe-manager/src/app/recipe-list/recipe-list.component.ts
+++ recipe-manager/src/app/recipe-list/recipe-list.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-recipe-list',
-  imports: [],
+  imports: [MatToolbarModule, MatCardModule, CommonModule],
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+recipes: any;
 
 }
