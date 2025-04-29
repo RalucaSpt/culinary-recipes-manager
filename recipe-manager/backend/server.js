b4fd6881533563fc88f4390a10c3392717682f8b
gabitzza <gabita.maria.15@gmail.com>
2025-04-29 21:23:10 +0300

    problem solved

--- recipe-manager/backend/server.js
+++ recipe-manager/backend/server.js
@@ -1,10 +1,8 @@
-
 const express = require('express');
 const cors = require('cors');
-const app = express();
-
 const recipeRoutes = require('./src/recipe.routes');
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/recipes', recipeRoutes);
@@ -12,12 +10,4 @@ app.use('/api/recipes', recipeRoutes);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Serverul rulează pe portul ${PORT}`);
-
-const app = require('./app');
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-
 });
